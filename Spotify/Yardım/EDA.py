2c129bfccc2e05475f45a76a9a390d67f30f3bab
Yusuf Cızlaşmak <ycizlasmak@gmail.com>
2023-05-13 01:36:11 +0300

    second dataset is completed by author

--- Spotify/Yardım/EDA.py
+++ Spotify/Yardım/EDA.py
@@ -15,11 +15,37 @@ def controlNaNvalues(data):
     print(data.isna().sum())
     print("-----\n")
     x=(data.isna().sum())/(data.shape[0])
-    df=pd.DataFrame(data=x)
+    df=pd.DataFrame({'Bos_Sutunların_oranı': x})
+    return df
     
 
 
 ## GÖRSELLEŞTİRME 
+#Daha kullanışlı.
+def BarPlot(data,degisken,n,color):
+    """datanızı temsil ederken , degisken: datadaki hangi sutunda gösterileceğini temsil eder ,n ise topN temsil etmektedir. En popüler kaç ismi görmek istersiniz gibi.
+     color ise color kodunu giriniz. """
+    
+    mostPopular=data[degisken].sort_values(ascending=False)
+
+    x=list(mostPopular.index[:n])
+    y=list(mostPopular.values[:n])
+
+    name=[]
+    for i in x:
+        name.append(data.loc[i,'track_name'])
+    
+    print(name)
+    plt.figure(figsize=(10,7))
+    plt.barh(name,y,label='Firmadaki En Popüler Sanatçılar',color=color)
+    plt.ylabel('Sanatçılar')
+    plt.xlabel('Popülerlik Düzeyi')
+    plt.title('Pointplot Chart')
+    plt.legend()
+    plt.show()
+
+
+
 def PopularBarPlot(data,n):
     """datanızı temsil ederken , n ise topN temsil etmektedir. En popüler kaç ismi görmek istersiniz gibi. """
     
@@ -34,6 +60,27 @@ def PopularBarPlot(data,n):
     
     print(name)
     plt.figure(figsize=(10,7))
-    plt.barh(name,y,color='#306632')
-
+    plt.barh(name,y,label='Firmadaki En Popüler Sanatçılar',color='#306632')
+    plt.xlabel('Sanatçılar')
+    plt.ylabel('Popülerlik Düzeyi')
+    plt.title('Pointplot Chart')
+    plt.legend()
     plt.show()
+
+
+def LoudnessBarPlot(data,n):
+    MostLoudness=data['loudness'].sort_values(ascending=False)
+
+    x=list(MostLoudness.index[:n])
+    y=list(MostLoudness.values[:n])
+
+
+    name=[]
+    for i in x:
+        name.append(data.loc[i,'track_name'])
+
+    plt.figure(figsize=(10,10))
+    sns.barplot(data=data, x=name, y=y,saturation = 0.1)
+    plt.xlabel('Şarkılar')
+    plt.ylabel('Gürültü Düzeyi')
+    plt.title('Pointplot Chart')
